d846d6d408b85daeb44782eacfe0d2e6644e4bf8
Jonathan Kim <jonnokim17@gmail.com>
2015-02-02 13:09:54 -0800

    Initial Commit

---
+++ TouchTracker/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  TouchTracker
+//
+//  Created by Jonathan Kim on 2/2/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Jonathan Kim on 2/2/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+    }
+}
